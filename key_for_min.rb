51532147da3aa0b4d0d0f395894e4d76e8d867ab
Edgar Ong <emailedgar@gmail.com>
2019-03-21 17:46:42 +0000

    reviewed and refactored

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
+require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 # hash = {:blake => 500, :ashley => 2, :adam => 1}
 # 2nd hash = {:blake => 10, :ashley => 50, :adam => 17}
-
 
 def key_for_min_value(name_hash)
   
@@ -11,41 +11,10 @@ def key_for_min_value(name_hash)
   low_value = nil
   
   name_hash.each do |name, value|
-    low_value = value + value
-  end
-
-  name_hash.each do |name, value|
-    if value < low_value
+    if low_value == nil || value < low_value
       low_name = name
       low_value = value
     end
   end
   low_name
 end
-
-#===with help===
-# def key_for_min_value(name_hash)
-#   #i want to take the value, compare the first value with the second value
-#   #if hash is empty return nil
-  
-#   new_value = nil
-#   new_name = ""
-  
-#   if name_hash.empty?
-#     new_value
-#   else
-#     name_hash.each do |name,value|
-#       if !new_value #this changes nil to true
-#                     #if there is already a value, it sets it to false
-#                     ##if false, it wont run
-#         new_value = value
-#         new_name = name
-#         elsif value < new_value
-#         new_value = value
-#         new_name = name
-#       end
-        
-#     end
-#     new_name
-#   end
-# end
